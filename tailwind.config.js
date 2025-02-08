d075876efb541f495c0c8030212188705019cf75
Amirhossein <amirhossein.br.sir@gmail.com>
2025-02-08 23:47:27 +0330

    refactor: update Tailwind CSS integration and clean up configuration files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      spacing:{
-        '25': '100px',
-        '45' : '185px'
-      },
-      fontFamily: {
-        lexend: ["Lexend", "sans-serif"], 
-      },
-    },
-  },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {},
   plugins: [],
-}
+};
